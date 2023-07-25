fedc88975d67e65ad4dd240658e9f44284e91a2a
scitechkz <92871137+scitechkz@users.noreply.github.com>
2023-07-25 23:32:55 +0100

    Update aws-config.js from Github

--- src/aws-config.js
+++ src/aws-config.js
@@ -1,10 +1,9 @@
-// aws-config.js
 import AWS from 'aws-sdk';
 
 AWS.config.update({
-  accessKeyId: '',
-  secretAccessKey: '',
-  region: 'us-east-1',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
 });
 
 export const s3 = new AWS.S3();
